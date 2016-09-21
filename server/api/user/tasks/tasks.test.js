750efbd20e0b92378b79d6611cbdbe91ac6dafc4
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-09-20 19:54:51 -0700

    Add before/after tags

--- server/api/user/tasks/tasks.test.js
+++ server/api/user/tasks/tasks.test.js
@@ -4,8 +4,34 @@ chai.use(require('chai-http'));
 let should = chai.should();
 
 let app = require('../../../server');
+let User = require('../../../models/user.model');
 
 describe('Requests on /:user/tasks', function() {
+	before(function(done) {
+		User.remove({}).exec();
+		User.create({
+			username: 'benjamin',
+			password: 'password',
+			tasks: [
+				{
+					title: 'My Super Awesome Task',
+					date: Date.now(),
+					time: (1000 * 40),
+					description: 'Lorem ipsum dolor sit.'
+				},
+				{
+					title: 'My Second Super Aweseom Task',
+					date: 2016-09-20,
+					time: (1000 * 100),
+					description: 'Lorem ipsum dolor sit.'
+				}
+			]
+		}, function(err, users) {
+			console.log(users);
+			done();
+		});
+	});
+
 	describe('/', function() {
 		it('GET -- username not in db returns 404', function(done) {
 			chai.request(app)
@@ -15,5 +41,9 @@ describe('Requests on /:user/tasks', function() {
 					done();
 				});
 		});
+	});
+
+	after(function(done) {
+		User.remove({}, done);
 	});
 });
