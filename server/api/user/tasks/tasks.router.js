630d729da37b8beff7fa7483fed8779ee5079df3
benjaminj6 <benjamin.d.johnson@icloud.com>
2016-09-23 00:24:46 -0700

    Implement all 4 CRUD operations on Tasks Endpoint

--- server/api/user/tasks/tasks.router.js
+++ server/api/user/tasks/tasks.router.js
@@ -5,7 +5,8 @@ let controller = require('./tasks.controller');
 
 // TO DO -- Eventually, remove :id from endpoint. Pass User ID through session data.
 router.get('/:id/tasks', controller.getAllTasks);
-router.delete('/:id/tasks/delete-:taskID', controller.deleteTask);
+router.delete('/:id/tasks/delete/:taskID', controller.deleteTask);
 router.post('/:id/tasks/create', controller.createTask);
+router.put('/:id/tasks/edit/:taskID', controller.editTask);
 
 module.exports = router;
--- server/api/user/tasks/tasks.test.js
+++ server/api/user/tasks/tasks.test.js
@@ -40,7 +40,7 @@ describe('Tasks Endpoint', function() {
 	});
 
 	describe('GET requests on /api/username/tasks', function() {
-		it('Valid -- returns 200 and user JSON', function(done) {
+		it('Valid: 200 -- gets all user info', function(done) {
 			chai.request(app)
 				.get('/api/1/tasks')
 				.end(function(err, res) {
@@ -54,7 +54,7 @@ describe('Tasks Endpoint', function() {
 				});
 		});
 
-		it('Invalid -- username not in db returns 404', function(done) {
+		it('Invalid: 404 -- username not in db', function(done) {
 			chai.request(app)
 				.get('/api/20/tasks/')
 				.end(function(err, res) {
@@ -69,7 +69,7 @@ describe('Tasks Endpoint', function() {
 	describe('DELETE requests on /api/id/tasks/delete-:taskID', function() {
 		it('Valid: 200 -- returns updated user', function(done) {
 			chai.request(app)
-				.delete('/api/1/tasks/delete-1')
+				.delete('/api/1/tasks/delete/1')
 				.end(function(err, res) {
 					should.equal(err, null);
 					res.should.have.status(200);
@@ -87,7 +87,7 @@ describe('Tasks Endpoint', function() {
 		}
 
 		it('Invalid: 404 -- wrong user id', function(done) {
-			chaiDelete('/api/20/tasks/delete-1')
+			chaiDelete('/api/20/tasks/delete/1')
 				.end(function(err, res) {
 					err.should.have.status(404);
 					res.should.be.json;
@@ -98,7 +98,7 @@ describe('Tasks Endpoint', function() {
 		});
 
 		it('Invalid: 404 -- wrong task id', function(done) {
-			chaiDelete('/api/1/tasks/delete-20')
+			chaiDelete('/api/1/tasks/delete/20')
 				.end(function(err, res) {
 					err.should.have.status(404);
 					res.should.be.json;
@@ -253,5 +253,132 @@ describe('Tasks Endpoint', function() {
 					done();
 				});
 		});
+	});
+
+	describe('PUT requests on /api/id/tasks/edit/taskID', function() {
+		it('Valid: 200 -- change title', function(done) {
+			let newTitle = { key: 'title', value: 'My New Title'};
+
+			chaiEdit(newTitle)
+				.end(function(err, res) {
+					should.equal(err, null);
+					res.should.have.status(200);
+					res.should.be.json;
+					res.body.should.be.a('Object');
+					res.body.title.should.equal('My New Title');
+					done();
+				});
+		});
+
+		it('Valid: 200 -- change description', function(done) {
+			let newDescription = {key: 'description', value: 'My New Description'};
+
+			chaiEdit(newDescription)
+				.end(function(err, res) {
+					should.equal(err, null);
+					res.should.have.status(200);
+					res.should.be.json;
+					res.body.should.be.a('Object');
+					res.body.description.should.equal('My New Description');
+					done();
+				});
+		});
+
+		it('Invalid: 400 -- input "time" as "key"', function(done) {
+			let invalidKey = {key: 'time', value: 'My New Time'};
+			
+			chaiEdit(invalidKey)
+				.end(function(err, res) {
+					err.should.have.status(400);
+					res.should.be.json;
+					res.body.name.should.equal('ValidationError');
+					res.body.message.should.equal('"key" must be one of [title, description]');
+					done();
+				});
+		});
+
+		it('Invalid: 400 -- no "key" input', function(done) {
+			let noKey = {value: 'My Description -- for what???'};
+
+			chaiEdit(noKey)
+				.end(function(err, res) {
+					err.should.have.status(400);
+					res.should.be.json;
+					res.body.name.should.equal('ValidationError');
+					res.body.message.should.equal('"key" is required');
+					done(); 
+				});
+		});
+
+		it('Invalid: 400 -- non-String as "value"', function(done) {
+			let nonStringValue = {key: 'title', value: {}};
+
+			chaiEdit(nonStringValue)
+				.end(function(err, res) {
+					err.should.have.status(400);
+					res.should.be.json;
+					res.body.name.should.equal('ValidationError');
+					res.body.message.should.equal('"value" must be a string');
+					done();
+				});
+		});
+
+		it('Invalid: 400 -- non-String as "key"', function(done) {
+			let nonStringKey = {key: {}, value: 'My Fancy String'};
+
+			chaiEdit(nonStringKey)
+				.end(function(err, res) {
+					err.should.have.status(400);
+					res.should.be.json;
+					res.body.name.should.equal('ValidationError');
+					res.body.message.should.equal('"key" must be a string');
+					done();
+				});
+		});
+
+		it('Invalid: 400 -- no "value" input', function(done) {
+			let noValue = {key: 'title'};
+
+			chaiEdit(noValue)
+				.end(function(err, res) {
+					err.should.have.status(400);
+					res.should.be.json;
+					res.body.name.should.equal('ValidationError');
+					res.body.message.should.equal('"value" is required');
+					done();
+				});
+		});
+
+		it('Invalid: 404 -- user id not in db', function(done) {
+			chai.request(app)
+				.put('/api/20/tasks/edit/1')
+				.send({ key: 'title', value: 'My New Title'})
+				.end(function(err, res) {
+					err.should.have.status(404);
+					res.should.be.json;
+					res.body.name.should.equal('NotFound');
+					res.body.message.should.equal('This user id was not found in the database');
+					done();
+				});
+		});
+
+		it('Invalid: 404 -- wrong taskId', function(done) {
+			chai.request(app)
+				.put('/api/1/tasks/edit/34')
+				.send({ key: 'title', value: 'My New title'})
+				.end(function(err, res) {
+					err.should.have.status(404);
+					res.should.be.json;
+					res.body.name.should.equal('NotFound');
+					res.body.message.should.equal('This task id was not found in the database');
+					done();
+				});
+		});
+
+		function chaiEdit(request) {
+			return chai.request(app)
+				.put('/api/1/tasks/edit/1')
+				.send(request);
+		}
 	});
 });
