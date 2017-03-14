e69c5130f4bb908c52642ee5c1ddc3ad578b4515
Benjamin Johnson <benjamin.d.johnson@icloud.com>
2017-03-14 16:23:20 -0700

    redesign: remove console.log()

--- build/js/home/app.js
+++ build/js/home/app.js
@@ -2,8 +2,6 @@ let swapClasses = require('../utils/swap.classes');
 
 module.exports = () => {
   $(document).ready(() => {
-    console.log('Hey there')
-
     $('.menu-icon').click(function() {
       let $this = $(this);
 
